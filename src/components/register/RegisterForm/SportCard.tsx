e000d14758e5a0f7a6090e4f5d1d3741f70f00a1
Maxime Roucher <maximeroucher@gmail.com>
2025-06-15 10:44:12 +0200

    feat: adding document input

--- src/components/register/RegisterForm/SportCard.tsx
+++ src/components/register/RegisterForm/SportCard.tsx
@@ -14,6 +14,18 @@ import {
 } from "../../ui/select";
 import { Checkbox } from "../../ui/checkbox";
 import { Input } from "../../ui/input";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../../ui/dialog";
+import { FormMessage } from "../../ui/form";
+import { Button } from "../../ui/button";
+import { LoadingButton } from "../../custom/LoadingButton";
+import { HiDownload } from "react-icons/hi";
+import { DocumentDialog } from "../../custom/DocumentDialog";
 
 interface SportCardProps {
   form: UseFormReturn<z.infer<typeof registeringFormSchema>>;
@@ -104,12 +116,77 @@ export const SportCard = ({ form, sports }: SportCardProps) => {
         )}
       />
 
-      <StyledFormField
-        form={form}
-        label="Numéro de licence"
-        id="sport.license_number"
-        input={(field) => <Input {...field} className=" lg:w-2/3 w-full" />}
-      />
+      <div className="flex flex-col gap-4 lg:flex-row w-2/3">
+        <StyledFormField
+          form={form}
+          label="Numéro de licence"
+          id="sport.license_number"
+          input={(field) => <Input {...field} className="w-full" />}
+        />
+        <StyledFormField
+          form={form}
+          label="Certificat médical"
+          id="sport.license_number"
+          input={(field) => (
+            <div className="col-span-4 gap-2 grid grid-cols-3">
+              <Dialog>
+                <FormMessage />
+                <DialogTrigger asChild>
+                  <Button
+                    variant="outline"
+                    className={"col-span-3"}
+                    // disabled={isUploading}
+                  >
+                    <div className="flex flex-row items-start w-full">
+                      {/* <>
+                    {field.value?.name ? (
+                      <span className="text-gray-500 overflow-hidden">
+                        {field.value.name ?? "Aucun fichier séléctionné"}
+                      </span>
+                    ) : ( */}
+                      <span className="font-semibold mr-6">
+                        Choisir un fichier
+                      </span>
+                      {/* )}
+                  </> */}
+                    </div>
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="md:max-w-2xl top-1/2">
+                  <DialogHeader>
+                    <DialogTitle className="text-red sm:text-lg">
+                      {/* {label} */}
+                    </DialogTitle>
+                  </DialogHeader>
+                  {/* <DocumentDialog
+                setIsOpen={setIsOpen}
+                setIsUploading={setIsUploading}
+                field={field}
+                fileType={id}
+                documentId={field.value?.id}
+                participantId={participantId!}
+              /> */}
+                </DialogContent>
+              </Dialog>
+              {/* {id === "raidRules" && !!information?.raid_rules_id && (
+            <LoadingButton
+              variant="outline"
+              className="col-span-1"
+              type="button"
+              onClick={(_) => downloadRaidRules(information.raid_rules_id!)}
+              isLoading={isFileLoading}
+              label={
+                <>
+                  <HiDownload className="mr-2" />
+                  Télécharger
+                </>
+              }
+            />
+          )} */}
+            </div>
+          )}
+        />
+      </div>
     </CardTemplate>
   );
 };
