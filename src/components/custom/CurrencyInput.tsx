feab875d37a706a62c6735e1b4651ff5537b5eca
Maxime Roucher <maximeroucher@gmail.com>
2025-09-15 21:47:55 +0200

    fix: using another lib to currency input

--- src/components/custom/CurrencyInput.tsx
+++ src/components/custom/CurrencyInput.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import MaskedInput from "react-text-mask";
+import InputMask from "react-input-mask";
 import createNumberMask from "text-mask-addons/dist/createNumberMask";
 
 interface CurrencyInputProps {
@@ -21,11 +21,10 @@ const defaultMaskOptions = {
   allowLeadingZeroes: false,
 };
 
-export const CurrencyInput: React.FC<CurrencyInputProps> = ({
-  value,
-  onChange,
-  id,
-}) => {
+export const CurrencyInput = React.forwardRef<
+  HTMLInputElement,
+  CurrencyInputProps
+>(({ value, onChange, id }, ref) => {
   const currencyMask = createNumberMask(defaultMaskOptions);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -35,7 +34,6 @@ export const CurrencyInput: React.FC<CurrencyInputProps> = ({
       event.nativeEvent.data === ","
     ) {
       rawValue += ".";
-      // Move the cursor to the end of the string
       const input = event.target as HTMLInputElement;
       const end = input.value.length;
       input.setSelectionRange(end, end);
@@ -46,15 +44,14 @@ export const CurrencyInput: React.FC<CurrencyInputProps> = ({
   };
 
   return (
-    <MaskedInput
+    <InputMask
       mask={currencyMask}
       value={value}
       onChange={handleChange}
       id={id}
       className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-      render={(ref, props) => (
-        <input ref={ref as React.Ref<HTMLInputElement>} {...props} />
-      )}
     />
   );
-};
+});
+
+CurrencyInput.displayName = "CurrencyInput";
