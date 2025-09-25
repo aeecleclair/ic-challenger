e13ae470785d36a0638c26335cda062db08ee5ec
Maxime Roucher <maximeroucher@gmail.com>
2025-09-25 20:25:25 +0200

    fix: match creation location mandatory

--- src/forms/match.ts
+++ src/forms/match.ts
@@ -6,10 +6,7 @@ export const matchFormSchema = z.object({
   team1_id: z.string().min(1, "L'équipe 1 est requise"),
   team2_id: z.string().min(1, "L'équipe 2 est requise"),
   date: z.date({ invalid_type_error: "La date est requise" }),
-  location_id: z.string({
-    required_error: "Le lieu est requis",
-    invalid_type_error: "Le lieu est requis",
-  }),
+  location_id: z.string().min(1, "Le lieu est requis"),
   score_team1: z.number().int().optional(),
   score_team2: z.number().int().optional(),
   winner_id: z.string().optional(),
