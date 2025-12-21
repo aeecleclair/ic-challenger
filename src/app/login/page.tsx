fce61057064a4fa7c7de19821719c46684af1465
maximeroucher <maximeroucher@gmail.com>
2025-12-21 11:27:12 +0100

    fix: removing unused space in login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,46 +1,24 @@
 "use client";
 
 import * as React from "react";
-import { GalleryVerticalEnd } from "lucide-react";
 import { LoginForm } from "@/src/components/login/LoginForm";
+import { Logo } from "@/src/components/custom/Logo";
 
 const Login = () => {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
+    <div className="grid min-h-svh">
       <div className="flex flex-col gap-8 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
+            <Logo />
             Challenger
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center py-12">
-          <div className="w-full max-w-md">
+          <div className="w-full max-w-sm">
             <LoginForm />
           </div>
         </div>
-        {/* <div className="text-muted-foreground text-center text-sm text-balance mt-auto">
-          En vous connectant, vous acceptez nos{" "}
-          <a
-            href="#"
-            className="underline underline-offset-4 hover:text-primary"
-          >
-            Conditions d&apos;utilisation
-          </a>{" "}
-          et notre{" "}
-          <a
-            href="#"
-            className="underline underline-offset-4 hover:text-primary"
-          >
-            Politique de confidentialité
-          </a>
-          .
-        </div> */}
-      </div>
-      <div className="relative hidden lg:block py-[3vh] pr-[3vh]">
-        <div className="bg-muted h-full rounded-xl overflow-hidden"></div>
       </div>
     </div>
   );
